8a741ec06654d518da12a10e818bc37e96dd8758
metamericdesign <devin@metameric.info>
2022-11-29 16:20:01 -0800

    usb

---
+++ files/etc/uci-defaults/90_resizewithusb.sh
@@ -0,0 +1,20 @@
+DEVICE="$(sed -n -e "/\s\/overlay\s.*$/s///p" /etc/mtab)"
+uci -q delete fstab.rwm
+uci set fstab.rwm="mount"
+uci set fstab.rwm.device="${DEVICE}"
+uci set fstab.rwm.target="/rwm"
+uci commit fstab
+echo y | mkfs.ext4 /dev/sda1
+
+DEVICE="/dev/sda1"
+eval $(block info "${DEVICE}" | grep -o -e "UUID=\S*")
+uci -q delete fstab.overlay
+uci set fstab.overlay="mount"
+uci set fstab.overlay.uuid="${UUID}"
+uci set fstab.overlay.target="/overlay"
+uci commit fstab
+mount /dev/sda1 /mnt
+
+tar -C /overlay -cvf - . | tar -C /mnt -xf -
+
+exit 0
---
+++ files/etc/uci-defaults/90_setup.sh
@@ -0,0 +1,8 @@
+cd /root 
+git clone https://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@github.com/metamericdesign/gsSystems_Lighting.git --depth 1
+cd /srv/www
+git clone https://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@github.com/metamericdesign/BaseStationPHP.git --depth 1
+
+chmod 777 /srv/www/BaseStationPHP/Php/LightingNodeLog.php
+
+exit 0
