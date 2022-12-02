c229d7fd2943cff1cd70f8bd196512f76d4faff7
metamericdesign <devin@metameric.info>
2022-12-01 18:20:08 -0800

    gitCloneRunning

--- files/root/rebootAfterFilesize.py
+++ files/root/rebootAfterFilesize.py
@@ -4,44 +4,90 @@ import os
 import syslog 
 
 path_to_usb = '/dev/sda1'
-
-
 path_to_oobCheck = '/root/oobCheck.txt'
+path_to_cloneCheck = '/root/gitCloneRequired.txt'
+path_to_cloneComplete = '/root/cloningComplete.txt'
+path_to_gsSystems_Lighting = '/root/gsSystems_Lighting'
+path_to_BaseStationPHP = '/srv/www/BaseStationPHP'
+
+gitlightingpath="/root"
+gitlighting="https://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@github.com/metamericdesign/gsSystems_Lighting.git"
+
+gitphppath="/srv/www" 
+gitphp="https://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@github.com/metamericdesign/BaseStationPHP.git"
+
+gitCloneRequested = False
 
 time.sleep(3)
 
 while(1):
     usbPath = Path(path_to_usb)
     oobPath = Path(path_to_oobCheck)
+    cloneCheckPath = Path(path_to_cloneCheck)
+    cloneCompletePath = Path(path_to_cloneComplete)
+    gsLightingPath = Path(path_to_gsSystems_Lighting)
+    baseStationPhpPath = Path(path_to_BaseStationPHP)
+
     usbPathexists = os.path.exists(usbPath)
     oobPathexists = os.path.exists(oobPath)
+    cloneRequired = os.path.exists(cloneCheckPath)
+    cloneComplete = os.path.exists(cloneCompletePath)
+    gsLightingPathexists = os.path.exists(gsLightingPath)
+    baseStationPhpPathexists = os.path.exists(baseStationPhpPath)
+
     syslog.syslog(f'Starting oobCheck')
     syslog.syslog(f' usbPathexists = {usbPathexists}')
     syslog.syslog(f' oobPathexists = {oobPathexists}')
 
 
-    if os.path.exists(usbPath) and oobPath.is_file():
+    if usbPathexists and oobPathexists: # checks if usb is connected and if a file resize is needed
         syslog.syslog(f'Starting file resize')
-        
-        os.system('echo "file resize complete" > /root/fileResizeComplete.txt')
+    
         os.system('rm /root/oobCheck.txt')
 
         os.system('./root/fileresize.sh')
-        time.sleep(20)
+        time.sleep(15)
 
         syslog.syslog(f'The file {oobPath} and {usbPath} exists, rebooting')
 
         os.system('reboot')
-        
-    elif os.path.exists(usbPath) and not oobPath.is_file():
-        syslog.syslog(f'OOB check complete, deleting unneeded files')
+
+    elif usbPathexists and not oobPathexists and not gitCloneRequested:  # checks if file resize is complete and call for a git clone
         syslog.syslog(f'The file {usbPath} exists and {oobPath} does not')
-        os.system('echo "oob check complete" > /root/oobCheckComplete.txt')
+        os.system('echo "file resize complete" > /root/fileResizeComplete.txt')
         os.system('echo "git clone required" > /root/gitCloneRequired.txt')
+        gitCloneRequested = True
+
+    elif cloneRequired: #checks if a git clone is required
+
+        syslog.syslog(f'Clone is required')
+
+        if gsLightingPath == False:
+            os.system('git -C {gitlightingpath} clone {gitlighting}')
+            syslog.syslog(f'gsLighting Clone aquired')
+
+        if baseStationPhpPath == False:
+            os.system('git -C {gitphppath} clone {gitphp}')
+            syslog.syslog(f'baseSationPhp Clone aquired')
+        
+        if gsLightingPath ==True and baseStationPhpPath == True:
+
+            time.sleep(7)
+            syslog.syslog(f'all Clones aquired')
+            os.system('rm /root/gitCloneRequired.txt')
+            os.system('echo "cloning is complete" > /root/cloningComplete.txt')
+
+    elif cloneCompletePath:
+        syslog.syslog(f'OOB check complete, deleting unneeded files')
+        syslog.syslog(f'The file {usbPath} and {cloneComplete} exist')
+        os.system('echo "oob check complete" > /root/oobCheckComplete.txt')
         os.system('rm /root/fileresize.sh')
         os.system('rm /etc/init.d/runOobCheck')
         os.system('rm /root/rebootAfterFilesize.py')
         os.system('reboot')
+
+    else:
     
-    syslog.syslog(f'Nothing found,going to sleep')
+        syslog.syslog(f'Nothing found,going to sleep')
+
     time.sleep(10)
