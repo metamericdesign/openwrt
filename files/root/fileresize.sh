d2e05ce6f95bf5d4a13481eef24313c7cb9cb27e
metamericdesign <devin@metameric.info>
2022-12-05 12:12:56 -0800

    oobState

--- files/root/fileresize.sh
+++ files/root/fileresize.sh
@@ -6,15 +6,15 @@ uci set fstab.rwm="mount"
 uci set fstab.rwm.device="${DEVICE}"
 uci set fstab.rwm.target="/rwm"
 uci commit fstab
-echo y | mkfs.ext4 /dev/sda1
+echo y | mkfs.ext4 /dev/mmcblk0p1
 
-DEVICE="/dev/sda1"
+DEVICE="/dev/mmcblk0p1"
 eval $(block info "${DEVICE}" | grep -o -e "UUID=\S*")
 uci -q delete fstab.overlay
 uci set fstab.overlay="mount"
 uci set fstab.overlay.uuid="${UUID}"
 uci set fstab.overlay.target="/overlay"
 uci commit fstab
-mount /dev/sda1 /mnt
+mount /dev/mmcblk0p1 /mnt
 
 tar -C /overlay -cvf - . | tar -C /mnt -xf -
