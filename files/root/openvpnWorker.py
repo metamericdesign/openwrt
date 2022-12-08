0d8cc6b38e57bcb2b22b91fa5b08740096d65a98
wloch-mick <ubuntu@DESKTOP-ASOGJHH.localdomain>
2022-12-07 22:29:14 -0600

    more better services

--- files/root/openvpnWorker.py
+++ files/root/openvpnWorker.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import time 
 import os
 import syslog 
@@ -5,80 +6,99 @@ import json
 from urllib import request
 from urllib.error import HTTPError
 
-hibernationTime = 15
-
 #all files path needed for checking
-path_to_systemFlags = '/root/systemStateFlags'
-path_to_provisionComplete = f'{path_to_systemFlags}/provisionComplete.txt'
-path_to_orgDetails = f'{path_to_systemFlags}/orgDetails.txt'
+path_to_provisionComplete = '/root/systemStateFlags/provisionComplete.txt'
+
 path_to_openvpnConfig = '/etc/config/openvpn'
 
-path_to_openvpn = '/dummypath' #dummy path to test as false for first pass through
+
+path_to_orgDetails = '/root/systemStateFlags/orgDetails.txt'
+orgDetailsPath = Path(path_to_orgDetails)
+
+path_to_openvpn = '/dummypath' #dummy path 
 
 #makes file paths into booleans
-
-time.sleep(15)
-
+provisionCompletePath = Path(path_to_provisionComplete)
+provisionCompletePathexists = os.path.exists(provisionCompletePath)
+syslog.syslog('VPN worker has started, waiting 15 seconds.')
+time.sleep(5)
 while(1):
+    try:
+        while(1):
+            syslog.syslog(' VPN Loop Start')
+            provisionCompletePathexists = os.path.exists(provisionCompletePath)
+            openvpnPath = Path(path_to_openvpn)
+            openvpnPathexists = os.path.exists(openvpnPath)
+            syslog.syslog(f'    provisionCompletePathexists = {provisionCompletePathexists}')
+            syslog.syslog(f'    openvpnPathexists = {openvpnPathexists}')
+            if (provisionCompletePathexists and not openvpnPathexists):
+                syslog.syslog('  VPN Download Required.')
+                try:
+                    f = open(orgDetailsPath, "r")
+                    orgFileContents = f.read()
+                    f.close()
+                    syslog.syslog('   JSON file read.')
+                    syslog.syslog(orgFileContents)
+                    try:
+                        orgDetails = json.loads(orgFileContents)
+                    except:
+                        syslog.syslog(syslog.LOG_ERR,'JSON file error!')
+                        break
+                    syslog.syslog('   JSON loaded.')
+                    openvpnUrl = orgDetails["ovpn_url"]
+                    base_num=orgDetails["base_num"]
+                    basestationName = f'base_{base_num}'
+                    path_to_openvpn = f'/etc/openvpn/{basestationName}.ovpn'
+                    with open('/root/systemStateFlags/uniqueID.txt', mode='r') as file:
+                            base_unique_id = file.read()
+                    syslog.syslog('Org details loaded.')
+                    #checks provisioning url to see if the device is allowed
+                    data_dict={"base_unique_id" : base_unique_id}
+                    json_data_encoded = json.dumps(data_dict).encode('utf-8')        
+                except err:
+                    syslog.syslog(syslog.LOG_ERR,'JSON load error!')
+                    break
+                try:
+                    # if allowed will get Org details and store to a file
+                    syslog.syslog('   Trying to download')
+                    req = request.Request(url=openvpnUrl)
+                    req.add_header('Content-Type', 'application/json; charset=utf-8')
+                    req.add_header('Content-Length', len(json_data_encoded))
+                    response = request.urlopen(req, json_data_encoded)
+                    if response.getcode() == 200:
+                        body = response.read().decode("utf-8")
+                        print(body)
+                        syslog.syslog(body)
+                        syslog.syslog('   Download Success, configuring service.')
+                        f = open(path_to_openvpn, "w")
+                        f.write(body)
+                        f.close()
 
-    provisionCompletePathexists = os.path.exists(path_to_provisionComplete)
-    openvpnPathexists = os.path.exists(path_to_openvpn)
+                        os.system('/etc/init.d/openvpn stop')
 
-    syslog.syslog(f'Github worker has started')
-    syslog.syslog(f'Checking for provisionComplete')
-    syslog.syslog(f'provisionCompletePathexists = {provisionCompletePathexists}')
+                        f = open(path_to_openvpnConfig, "w")
+                        f.write(f"config openvpn '{basestationName}'")
+                        f.write(f"\n\toption config '/etc/openvpn/{basestationName}.ovpn'")
+                        f.write(f"\n\toption enabled '1'")
+                        f.close()
+                        syslog.syslog('   Restarting openvpn.')
+                        os.system('/etc/init.d/openvpn restart')
+                    else:
+                        syslog.syslog(syslog.LOG_ERR,'Download Failed.')
+                        
+                #if problem occurs or device not allowed errors out
+                except HTTPError as err:
+                    syslog.syslog(f"Erroneous response: {err} - no connection to provisioning url")
+                    print(f"Erroneous response: {err} - no connection to provisioning url")
+            else:
+                syslog.syslog('  VPN already setup.')
+            
+            
+            syslog.syslog(' Loop End, sleeping 2 mins.')    
+            time.sleep(120)
+    except:
+        syslog.syslog(syslog.LOG_ERR,'Loop crash! Sleeping 2 mins.')
+    syslog.syslog('Loop exit. Sleeping 2 mins.')
+    time.sleep(20)    
 
-    if (provisionCompletePathexists and not openvpnPathexists):
-
-        f = open(path_to_orgDetails, "r")
-        orgDetails = json.loads(f.read())
-        f.close()
-
-        openvpnUrl = orgDetails["ovpn_url"]
-
-
-        base_num=orgDetails["base_num"]
-        basestationName = f'base_{base_num}'
-        path_to_openvpn = f'/etc/openvpn/{basestationName}.ovpn'
-
-
-        with open('/root/systemStateFlags/uniqueID.txt', mode='r') as file:
-                base_unique_id = file.read()
-
-        #checks provisioning url to see if the device is allowed
-        data_dict={"base_unique_id" : base_unique_id}
-        json_data_encoded = json.dumps(data_dict).encode('utf-8')
-        req = request.Request(url=openvpnUrl)
-        req.add_header('Content-Type', 'application/json; charset=utf-8')
-        req.add_header('Content-Length', len(json_data_encoded))
-
-        try:
-            # if allowed will get Org details and store to a file
-            response = request.urlopen(req, json_data_encoded)
-            if response.getcode() == 200:
-                body = response.read().decode("utf-8")
-                print(body)
-                syslog.syslog(body)
-
-                f = open(path_to_openvpn, "w")
-                f.write(body)
-                f.close()
-
-                os.system('/etc/init.d/openvpn stop')
-
-                f = open(path_to_openvpnConfig, "w")
-                f.write(f"config openvpn '{basestationName}'")
-                f.write(f"\n\toption config '/etc/openvpn/{basestationName}.ovpn'")
-                f.write(f"\n\toption enabled '1'")
-                f.close()
-
-                os.system('/etc/init.d/openvpn restart')
-
-                hibernationTime = 1800
-
-         #if problem occurs or device not allowed errors out
-        except HTTPError as err:
-            syslog.syslog(f"Erroneous response: {err} - no connection to provisioning url")
-            print(f"Erroneous response: {err} - no connection to provisioning url")
-
-    time.sleep(hibernationTime)
+syslog.syslog(syslog.LOG_ERR,'VPN Worker END.')
--- files/root/provisionCheck.py
+++ files/root/provisionCheck.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import time 
 import os
 import syslog 
@@ -5,69 +6,87 @@ import json
 from urllib import request
 from urllib.error import HTTPError
 
-hibernationTime = 15
-
 #all file paths needed for checking
-path_to_systemFlags = '/root/systemStateFlags'
-path_to_hardwareOobComplete = f'{path_to_systemFlags}/hardwareOobComplete.txt'
-path_to_provisionComplete = f'{path_to_systemFlags}/provisionComplete.txt'
-path_to_orgDetails = f'{path_to_systemFlags}/orgDetails.txt'
+syslog.syslog('Provisioning Service Started, waiting 10 seconds.')
+time.sleep(10)
+path_to_hardwareOobComplete = '/root/systemStateFlags/hardwareOobComplete.txt'
+hardwareOobCompletePath = Path(path_to_hardwareOobComplete)
+hardwareOobCompletePathexists = os.path.exists(hardwareOobCompletePath)
+
+path_to_provisionComplete = '/root/systemStateFlags/provisionComplete.txt'
+provisionCompletePath = Path(path_to_provisionComplete)
+
+path_to_orgDetails = '/root/systemStateFlags/orgDetails.txt'
+orgDetailsPath = Path(path_to_orgDetails)
+
+
+syslog.syslog(f' hardwareOobCompletePathexists = {hardwareOobCompletePathexists}')
 
 prov_url = 'http://gsvpn.eastus.cloudapp.azure.com/provision.php'
-
-time.sleep(10)
-
 while(1):
+        syslog.syslog(" Provisioning loop start.")
+        try:
+            hardwareOobCompletePathexists = os.path.exists(hardwareOobCompletePath)
 
-    hardwareOobCompletePathExists = os.path.exists(path_to_hardwareOobComplete)
+            #only starts if file resize has occured
+            if (hardwareOobCompletePathexists):
+                provisionCompletePathexists = os.path.exists(provisionCompletePath)
+                orgDetailsPathexists = os.path.exists(orgDetailsPath)
 
-    syslog.syslog(f'Checking for hardwareOobComplete_path')
-    syslog.syslog(f'hardwareOobCompletePathExists = {hardwareOobCompletePathExists}')
+                #only runs if base station has not been provisioned
+                if (not provisionCompletePathexists):
 
-    #only starts if file resize has occured
-    if (hardwareOobCompletePathExists):
+                    syslog.syslog(" Starting Provisioning process")
 
-        provisionCompletePathExists = os.path.exists(path_to_provisionComplete)
-        orgDetails_pathexists = os.path.exists(path_to_orgDetails)
+                    #gets the unique ID stored on the device
+                    with open('/root/systemStateFlags/uniqueID.txt', mode='r') as file:
+                            base_unique_id = file.read()
 
-        #only runs if base station has not been provisioned
-        if (not provisionCompletePathExists):
+                    #checks provisioning url to see if the device is allowed
+                    data_dict={"base_unique_id" : base_unique_id}
+                    try:
+                        json_data_encoded = json.dumps(data_dict).encode('utf-8')
+                        req = request.Request(url=prov_url)
+                        req.add_header('Content-Type', 'application/json; charset=utf-8')
+                        req.add_header('Content-Length', len(json_data_encoded))
+                        # if allowed will get Org details and store to a file
+                        response = request.urlopen(req, json_data_encoded)
+                        if response.getcode() == 200:
+                            body = response.read().decode("utf-8")
+                            print(body)
+                            syslog.syslog(body)
+                            
 
-            syslog.syslog("Starting Provisioning process")
 
-            #gets the unique ID stored on the device
-            with open('/root/systemStateFlags/uniqueID.txt', mode='r') as file:
-                    base_unique_id = file.read()
+                            #TODO additional checks on data from server, test JSON
 
-            #checks provisioning url to see if the device is allowed
-            data_dict={"base_unique_id" : base_unique_id}
-            json_data_encoded = json.dumps(data_dict).encode('utf-8')
-            req = request.Request(url=prov_url)
-            req.add_header('Content-Type', 'application/json; charset=utf-8')
-            req.add_header('Content-Length', len(json_data_encoded))
 
-            try:
-                # if allowed will get Org details and store to a file
-                response = request.urlopen(req, json_data_encoded)
-                if response.getcode() == 200:
-                    body = response.read().decode("utf-8")
-                    syslog.syslog(body)
+                            #stores org deatils
+                            f = open(orgDetailsPath, "w")
+                            f.write(body)
+                            f.close()
+                            
+                            #flag for provisioning complete
+                            f = open(provisionCompletePath, "w")
+                            f.write("provision complete")
+                            f.close()
+                            syslog.syslog(" Provisioning Success.")
 
-                    #stores org deatils
-                    f = open(path_to_orgDetails, "w")
-                    f.write(body)
-                    f.close()
-                    
-                    #flag for provisioning complete
-                    f = open(path_to_provisionComplete, "w")
-                    f.write("provision complete")
-                    f.close()
-
-                    hibernationTime = 1800 #sets longer wait if complete
-
-            #if problem occurs or device not allowed errors out
-            except HTTPError as err:
-                syslog.syslog(f"Erroneous response: {err} - no connection to provisioning url")
-                print(f"Erroneous response: {err} - no connection to provisioning url")
-
-    time.sleep(hibernationTime)
+                    #if problem occurs or device not allowed errors out
+                    except HTTPError as err:
+                        syslog.syslog(f"Erroneous response: {err} - no connection to provisioning url")
+                        print(f"Erroneous response: {err} - no connection to provisioning url")
+                    syslog.syslog(" Sleep 20 seconds.")
+                    time.sleep(20)
+                else :
+                    syslog.syslog(" Device alread provisioned, exit.")
+                    break
+            else:
+                syslog.syslog(" Waiting on file system resize.")
+                time.sleep(10)
+        except:
+            syslog.syslog("Provisioning Service Crash!")
+            time.sleep(20)
+        syslog.syslog(" Provisioning loop END. Wait 10 seconds.")
+        time.sleep(10)
+syslog.syslog(" Provisioning service exit.")
