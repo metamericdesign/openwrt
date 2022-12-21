b939e4805ec06ee0aa3dd65f9b8ea96845c25a36
metamericdesign <devin@metameric.info>
2022-12-21 15:28:41 -0800

    provision

--- files/root/serviceHandler.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-path_to_services = '/etc/init.d/'
-
-stat = subprocess.check_output("ps | grep py", shell=True).decode().strip()#binary->str->get rid of \n
-
-print(stat)
