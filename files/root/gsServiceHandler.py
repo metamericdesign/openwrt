73dd700261c3cd99dafce593d86d64c831763d0d
metamericdesign <devin@metameric.info>
2022-12-21 17:28:43 -0800

    ServiceHandler_WIP

--- files/root/gsServiceHandler.py
+++ files/root/gsServiceHandler.py
@@ -9,26 +9,33 @@ time.sleep(50)
 path_to_services = '/etc/init.d/'
 path_to_proccesses = '/root/'
 
-# all required workers for every base station
+# all required workers/services for every base station
 gsPreInstalledWorkers = ["gsHardwareOobWorker","gsProvisionWorker","gsOpenvpnWorker","gsGithubWorker"]
+gsPreInstalledServices = ["gsHardwareOobService" , "gsProvisionService" , "gsOpenvpnService" , "gsGithubService"]
 
-# any bought workers - loaded from the "software.txt"
+# any bought workers/services - loaded from the "software.txt"
 gsLicensedWorkers = ["win_light"] #not hard coded.
+gsLicensedServices = ["gsLightingService"]
 
-#combines all current Workers 
-gsWorkers = gsPreInstalledWorkers + gsLicensedWorkers
-gsServices = ["gsHardwareOobService" , "gsProvisionService" , "gsOpenvpnService" , "gsGithubService" , "gsLightingService" ]
+#combines all current Workers\/services 
+gsWorkersToCheck = gsPreInstalledWorkers + gsLicensedWorkers
+gsServicesToCheck = gsPreInstalledServices + gsLicensedServices
+
+#quick change for the column we want returned
 awkPrint = "{ print $1,$4,$5,$6 }"
 
 
 while(1):
     WorkersToStart=[]
     ServicesToStart=[]
-    for (service,worker) in zip(gsServices , gsWorkers):
+
+    print(f"gsWorkers to check = {gsWorkersToCheck}")
+    print(f"gsServices to check = {gsServicesToCheck}")
+    for (service,worker) in zip(gsServicesToCheck , gsWorkersToCheck):
         try:
             tempWorker= worker
             tempService= service
-            print(f'workers = {worker}')
+            print(f'worker = {worker}')
             tempString = worker
             stat = subprocess.check_output(f"ps | grep {worker} |  awk '{awkPrint}' | grep -v grep | grep -v /bin/sh ", shell=True).decode().strip()#binary->str->get rid of \n
             stat_list = (stat.split(" "))
@@ -43,16 +50,19 @@ while(1):
             print('-------------------------------------------------------')
 
 
-    print(WorkersToStart)
-    print(ServicesToStart)
+    print(f"WorkersToStart = {WorkersToStart}")
+    print(f"ServicesToStart = {WorkersToStart}")
+
     for (runWorker,RunService) in zip(WorkersToStart,ServicesToStart):
         try:
             completed_process = subprocess.run([f"/etc/init.d/{RunService}","start"])
-            print("I tried restarting")
+            print(f"Tried restarting {RunService}")
+            print(completed_process)
+            print('=======================================================')
 
         except Exception as err:
             print(f"ERROR {err}.")
-            print(f"I Failed")
+            print(f"Failed to restart {RunService}")
     
     time.sleep(15)
 
