3cb0ee909e8ca591adbec3a03aab4fc495981bfe
Mick <54990772+mick-wloch@users.noreply.github.com>
2023-01-03 00:42:16 -0600

    Update gsVPNProvisionWorker.py
    Signed-off-by: Mick <54990772+mick-wloch@users.noreply.github.com>

--- files/root/gsVPNProvisionWorker.py
+++ files/root/gsVPNProvisionWorker.py
@@ -1,6 +1,22 @@
 import os
 import syslog 
 
+hibernationTime = 30;
 
-While(1)
+path_to_requestroot = '/tmp/gsvpnrequests'
+path_to_basestation_requests = f'{path_to_requestroot}/basestation'
+path_to_desktop_requests = f'{path_to_requestroot}/desktop'
 
+While(1):
+  syslog.syslog('VPN Provision Worker Loop Start')
+  try:
+    while(1):
+      syslog.syslog('  Checking for new request files...')
+      # make sure temporary folders exist
+      
+      time.sleep(hibernationTime)
+            
+  except:
+    syslog.syslog(syslog.LOG_ERR,'VPN Provision Main loop crash!')
+  syslog.syslog('VPN Provision Worker Loop End, sleeping.')
+  time.sleep(hibernationTime)
