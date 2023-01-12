a59e5128ab7e9e849e454ec96efa15b563bd26bd
wloch-mick <ubuntu@DESKTOP-ASOGJHH.localdomain>
2023-01-12 11:22:16 -0600

    update hostname and server address

--- files/root/gsProvisionWorker.py
+++ files/root/gsProvisionWorker.py
@@ -49,10 +49,11 @@ def applyNetworkConfig(base_num, network_number):
     gsdb.gsDebugPrint('    Applying Network Number.')
     os.system(f'uci set network.lan.ipaddr="172.16.{base_num}.1"') # IP Address
     os.system('uci commit network')
-    os.system(f'uci set system.@system[0].hostname="base_{base_num}"') # HOSTNAME 
+    os.system(f'uci set system.@system[0].hostname="org{network_number}-base_{base_num}"') # HOSTNAME 
     os.system('uci commit system')
-    os.system(f'uci add_list dhcp.@dnsmasq[0].address="/orgdb.cloud/10.0.{network_number}.1"') # DATABASE CLOUD SERVER
+    os.system(f'uci add_list dhcp.@dnsmasq[0].address="/dbserver.lan/10.0.{network_number}.1"') # DATABASE CLOUD SERVER
     os.system('uci commit dhcp')
+
     # Restart services 
     gsdb.gsDebugPrint("    reloading system")
     os.system('/etc/init.d/system reload')
